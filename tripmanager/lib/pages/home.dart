7620094729eadf93671e2b0aecdc19e77aca5fe6
sakshamg2 <strippedghoul22@gmail.com>
2023-09-01 18:01:59 +0545

    serching places

--- tripmanager/lib/pages/home.dart
+++ tripmanager/lib/pages/home.dart
@@ -1,24 +1,104 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tripmanager/model/recommendation.dart';
 import 'package:tripmanager/pages/feed.dart';
 import 'package:get/get.dart';
+import 'package:tripmanager/pages/recommendation_details.dart';
 import 'package:tripmanager/pages/setting.dart';
 import 'package:tripmanager/pages/help.dart';
 import '../route/routes_names.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  _HomePageState createState() => _HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
+  final TextEditingController _queryController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  RecommendationResponse? recommendation;
+  bool isFetchingRecommendations = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
+      persistentFooterButtons: [
+        Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: TextField(
+              controller: _queryController,
+              decoration: InputDecoration(
+                hintText: 'Search..',
+                hintStyle: const TextStyle(color: Colors.grey),
+                labelStyle: const TextStyle(color: Color(0xFF0081A7)),
+                border: InputBorder.none,
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.send_sharp),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      setState(() {
+                        isFetchingRecommendations = true;
+                      });
+                      var url = Uri.parse(
+                          'http://127.0.0.1:8000/api/recommendation/recommendations/');
+
+                      var body = json.encode({
+                        "query": _queryController.text,
+                      });
+                      var prefs = await SharedPreferences.getInstance();
+                      final token = prefs.getString('token');
+                      http.Response response = await http.post(
+                        url,
+                        headers: {
+                          'Authorization': 'Bearer $token',
+                          'Content-Type': 'application/json'
+                        },
+                        body: body,
+                      );
+                      dynamic jsonResponse = jsonDecode(response.body);
+                      if (response.statusCode >= 200 &&
+                          response.statusCode < 300) {
+                        RecommendationResponse recommendationResponse =
+                            RecommendationResponse.fromJson(jsonResponse);
+                        setState(() {
+                          recommendation = recommendationResponse;
+                        });
+                      }
+                      setState(() {
+                        isFetchingRecommendations = false;
+                      });
+                    }
+                  },
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide(
+                    width: 2,
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: const BorderSide(
+                      width: 2,
+                      color: Color(0xFF0081A7),
+                    )),
+              ),
+            ),
+          ),
+        ),
+      ],
       appBar: AppBar(
         backgroundColor: const Color(0xFF0081A7),
         title: const Text(
@@ -43,68 +123,31 @@ class _HomePageState extends State<HomePage> {
             child: Container(
               color: Colors.grey[200],
               padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-              child: const Center(
-                child: Text(
-                  'Demo search result',
-                  style: TextStyle(
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Container(
-            height: 70,
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
-                    child: Container(
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.fromLTRB(20, 5, 10, 0),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                  hintText: 'Search..',
-                                  hintStyle:
-                                      const TextStyle(color: Colors.grey),
-                                  labelStyle:
-                                      const TextStyle(color: Color(0xFF0081A7)),
-                                  border: InputBorder.none,
-                                  suffixIcon: IconButton(
-                                    icon: const Icon(Icons.send_sharp),
-                                    onPressed: () {
-                                      // handle send button press
-                                    },
-                                  ),
-                                  enabledBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(10.0),
-                                    borderSide: BorderSide(
-                                      width: 2,
-                                      color: Colors.grey.shade400,
-                                    ),
-                                  ),
-                                  focusedBorder: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(10.0),
-                                      borderSide: const BorderSide(
-                                        width: 2,
-                                        color: Color(0xFF0081A7),
-                                      )),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
+              child: Builder(builder: (context) {
+                if (isFetchingRecommendations) {
+                  return const Center(
+                      child: CircularProgressIndicator.adaptive());
+                }
+                if (recommendation == null) {
+                  return const Center(child: Text('Try searching'));
+                }
+                return ListView.builder(
+                  itemCount: recommendation!.recommendations.length,
+                  itemBuilder: (context, index) {
+                    final recommend = recommendation!.recommendations[index];
+                    return ListTile(
+                      onTap: () => Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              RecommendationDetails(recommendation: recommend),
+                        ),
                       ),
-                    ),
-                  ),
-                ),
-              ],
+                      title: Text(recommend.name),
+                      subtitle: Text(recommend.description, maxLines: 3),
+                    );
+                  },
+                );
+              }),
             ),
           ),
         ],
